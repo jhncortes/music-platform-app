6adbe7138e84cd0eac999b6ab34e3cd27b9bc01a
jhncortes <jhn_cortes@outlook.com>
2025-08-19 20:48:57 +1000

    Added login and signup pages

--- frontend/src/components/GuestLayout.vue
+++ frontend/src/components/GuestLayout.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts"></script>
 
 <template>
-  <slot></slot>
+  <!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-900">
+  <body class="h-full">
+  ```
+-->
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+        alt="Your Company"
+        class="mx-auto h-10 w-auto"
+      />
+    </div>
+    <slot></slot>
+  </div>
 </template>
 
 <style scoped></style>
--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,9 +1,80 @@
 <script setup lang="ts">
+import { RouterLink } from "vue-router";
 import GuestLayout from "../components/GuestLayout.vue";
 </script>
 
 <template>
-  <GuestLayout>Login</GuestLayout>
+  <GuestLayout>
+    <h2
+      class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white"
+    >
+      Sign in to your account
+    </h2>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form action="#" method="POST" class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm/6 font-medium text-gray-100"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              id="email"
+              type="email"
+              name="email"
+              required
+              autocomplete="email"
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="password"
+              class="block text-sm/6 font-medium text-gray-100"
+              >Password</label
+            >
+            <div class="text-sm">
+              <a
+                href="#"
+                class="font-semibold text-indigo-400 hover:text-indigo-300"
+                >Forgot password?</a
+              >
+            </div>
+          </div>
+          <div class="mt-2">
+            <input
+              id="password"
+              type="password"
+              name="password"
+              required
+              autocomplete="current-password"
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+      <p class="mt-10 text-center text-sm/6 text-gray-400">
+        Not a member?
+        <RouterLink
+          :to="{ name: 'Signup' }"
+          class="font-semibold text-indigo-400 hover:text-indigo-300"
+        >
+          Sign up here
+        </RouterLink>
+      </p>
+    </div>
+  </GuestLayout>
 </template>
 
 <style scoped></style>
